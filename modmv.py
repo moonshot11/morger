ff215a778e7ec566fd5cdc2badad3f8df25d53fb
Moonshot <moonshot11@users.noreply.github.com>
2021-03-28 12:32:18 -0700

    Initial swap function

--- modmv.py
+++ modmv.py
@@ -9,9 +9,8 @@ ORIG_FOLDER = "ORIGINAL_FILES_BACKUP"
 MOD_FOLDER = "PUT_MOD_FILES_IN_THIS_FOLDER"
 
 class Entry:
-    def __init__(self, basepath, bakpath, status):
+    def __init__(self, basepath, status):
         self.basepath = basepath
-        self.bakpath = bakpath
         self.status = status
 
 class Config:
@@ -22,7 +21,7 @@ class Config:
         self.filename = filename
         self.entries = dict()
         entry = None
-        title, base, bak, status = None, None, None, None
+        title, base, status = None, None, None
         with open(filename, "r") as fin:
             for line in fin.readlines():
                 line = line.strip()
@@ -37,15 +36,15 @@ class Config:
                     k, v = match.groups()
                     if k == "GamePath":
                         basepath = v
-                    elif k == "ModPath":
-                        bakpath = v
                     elif k == "Active":
                         status = v
 
                 if not line and title:
-                    entry = Entry(basepath, bakpath, status)
+                    entry = Entry(basepath, status)
                     self.entries[title] = entry
-                    title, basepath, bakpath, status = None, None, None, None
+                    if status not in ("Yes", "No"):
+                        print("-W- Invalid status found:", status)
+                    title, basepath, status = None, None, None
 
     def write(self):
         """Write out config file"""
@@ -53,9 +52,38 @@ class Config:
             for title, entry in self.entries.items():
                 fout.write(f"[{title}]\n")
                 fout.write(f"    GamePath = {entry.basepath}\n")
-                fout.write(f"    ModPath  = {entry.bakpath}\n")
                 fout.write(f"    Active   = {entry.status}\n")
                 fout.write("\n")
+
+def modswap(args, entry):
+    """Install or uninstall a mod"""
+    title = args.swap
+    bakpath = os.path.join(args.modpath, title, MOD_FOLDER)
+    origpath = os.path.join(args.modpath, title, ORIG_FOLDER)
+    gamepath = entry.basepath
+
+    if entry.status == "Yes":
+        dest1 = bakpath
+        src2 = origpath
+    elif entry.status == "No":
+        dest1 = origpath
+        src2 = bakpath
+    src1 = dest2 = gamepath
+
+    for dpath, dnames, fnames in os.walk("."):
+        for fname in fnames:
+            fpath = os.path.join(dpath, fname)
+            fpath = os.path.relpath(fpath, start=src2)
+            os.renames(
+                os.path.join(src1, fpath),
+                os.path.join(dest1, fpath)
+            )
+            os.renames(
+                os.path.join(src2, fpath),
+                os.path.join(dest2, fpath)
+            )
+
+    entry.status = "No" if entry.status == "Yes" else "Yes"
 
 def setup_args():
     """Initialize arguments"""
@@ -70,7 +98,7 @@ def setup_args():
 
     meg_mode.add_argument("--list", action="store_true")
     meg_mode.add_argument("--init")
-    meg_mode.add_argument("--swap")
+    meg_mode.add_argument("--swap", "-s")
 
     return parser.parse_args()
 
@@ -81,7 +109,6 @@ if __name__ == "__main__":
         for k, v in config.entries.items():
             print(k)
             print("    Game Path = " + v.basepath)
-            print("    Mod Path  = " + v.bakpath)
             print("    Mod active? " + v.status)
             print()
     elif args.init:
@@ -93,5 +120,6 @@ if __name__ == "__main__":
         config.write()
         os.makedirs(os.path.join(args.modpath, args.init, ORIG_FOLDER))
         os.makedirs(os.path.join(args.modpath, args.init, MOD_FOLDER))
-    else:
+    elif args.swap:
+        modswap(args, config.entries[args.swap])
         config.write()
